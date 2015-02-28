26d535548f153f5784d931e704422d0352e2af20
Janani Thiru <janani.thiru@gmail.com>
2015-02-28 01:45:56 -0600

    HelloWorld_MPI.c

---
+++ rs_mpi/Debug/src/rs_mpi.d
@@ -0,0 +1,6 @@
+src/rs_mpi.d: ../src/rs_mpi.c /usr/include/openmpi-x86_64/mpi.h \
+ /usr/include/openmpi-x86_64/mpi_portable_platform.h
+
+/usr/include/openmpi-x86_64/mpi.h:
+
+/usr/include/openmpi-x86_64/mpi_portable_platform.h:
